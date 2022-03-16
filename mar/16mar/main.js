079c8011d597a9921403a7a2112b2858a8dee02b
sgujuluva <sanghee04@gmail.com>
2022-03-16 17:26:12 +0100

    practice

---
+++ mar/16mar/main.js
@@ -0,0 +1,15 @@
+console.log("hello");
+console.log(5);
+console.log("100"+5);
+console.log("100"+"5");
+console.log("100"-5);
+console.log("100"*5);
+console.log("100"/5);
+
+
+
+let firstName ="sangee";
+console.log(firstName);
+/*  let firstName="srrie";
+console.log(firstName); */
+ 
