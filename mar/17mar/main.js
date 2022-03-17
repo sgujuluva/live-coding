90c56f74b0caa924bd009ebce04b41ceccb41be1
sgujuluva <sanghee04@gmail.com>
2022-03-17 14:00:41 +0100

    today's practice

---
+++ mar/17mar/main.js
@@ -0,0 +1,20 @@
+let pears = 1;
+pears += 2*3;
+console.log(pears);
+
+/* length of the string */
+let greeting ="hello";
+console.log(greeting.length);
+/* another way of taking lenght */
+console.log("hey".length);
+
+/* string index */
+let sentence = "i am a good girl";
+console.log(sentence.length);
+console.log(sentence[16-2]);
+console.log(sentence[sentence.length-1]);
+
+/* template literal strings */
+let firstName = "srrie";
+let lastName = "wershan";
+console.log(`My son name is ${firstName} ${lastName}`);
