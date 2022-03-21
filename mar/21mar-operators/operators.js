91929398e8ca74b4bae08be6e353203cf06738b5
sgujuluva <sanghee04@gmail.com>
2022-03-21 13:36:24 +0100

    practice

--- mar/21mar-operators/operators.js
+++ mar/21mar-operators/operators.js
@@ -21,3 +21,27 @@ console.log('is "hello" equal to "Hello" ?', "hello" === "Hello"); //case sensit
 console.log(`is "2" equal to 2?`, "2" === 2); // false..... string is compared to a number
 
 console.log(`is "2" equal to 2?, "2" == 2`); // true ... 2 equal signs will convert string to number and compares the value
+
+
+//properties --- length
+let greeting = "hello world";
+console.log(greeting.length);
+
+/* string methods */
+// method - touppercase()
+
+console.log(greeting.toUpperCase()); // HELLO WORLD
+console.log("hey".toUpperCase()); // HEY
+
+// substring --method // start ---start exact index...... end index- letter wanted index + 1
+let word = "hello world";
+console.log(word.substring(0,5)); // hello
+console.log(word.substring(0,4)); // hell
+console.log(word.substring(2,5)); // llo
+console.log(word.substring(6,11)); // world
+console.log(word.substring(0)); // hello world
+
+console.log(word.slice(2,5)); // llo
+console.log(word.slice(0,4)); // hell
+console.log(word);
+
