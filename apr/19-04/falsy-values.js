a9ea2602cefd85b3c450816325f8bdbc2297f969
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2022-04-20 15:53:37 +0200

    solution

---
+++ apr/19-04/falsy-values.js
@@ -0,0 +1,18 @@
+let string = "h";
+let firstLetter = string[0];
+
+if (firstLetter && firstLetter.toUpperCase() === firstLetter) {
+  console.log(`OK: first letter is uppercase: ${firstLetter}`);
+} else {{
+  console.log(`NO`);
+}
+  console.log(`NO`);
+}
+
+// if (firstLetter === undefined) {
+//   console.log("firstletter is undefined");
+// } else if (firstLetter.toUpperCase() === firstLetter) {
+//   console.log("first letter is uppercase");
+// } else {
+//   console.log("first letter is NOT uppercase");
+// }
