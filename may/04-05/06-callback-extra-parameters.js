2597a84f0e959b6c7f039dd7d31b64956f79f901
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2022-05-04 22:29:54 +0200

    add explanation to readme

--- may/04-05/06-callback-extra-parameters.js
+++ may/04-05/06-callback-extra-parameters.js
@@ -1,7 +1,7 @@
 let numbers = [1, 2, 3, 4, 5];
 
 function double(n) {
-    return n * 2;
+  return n * 2;
 }
 
 // function triple(n, index, array) {
@@ -13,15 +13,47 @@ function double(n) {
 // console.log(numbers.map(triple));
 
 function multiplyArrayBy(array, multiplier) {
-    let result = array.map((n) => n * multiplier);
-    return result;
+  let result = array.map((n) => n * multiplier);
+  return result;
 }
 
 console.log(multiplyArrayBy(numbers, 2));
 console.log(multiplyArrayBy(numbers, 3));
 console.log(multiplyArrayBy(numbers, 4));
 
-
-
 let mapResult = numbers.map((n) => n / 2);
 console.log(mapResult);
+
+let property = "background-color";
+
+console.log(property.split("-"));
+
+
+
+console.log(numberAdd("iam2015bar6cat1")); //15
+console.log(numberAdd("In 2015, I want to know how much does iPhone 6+ cost"));
+console.log(numberAdd("In  I want to know how much does iPhone + cost?"));
+
+function camelize(str) {
+  //write your code here
+  let strArray = str.split("-");
+  let result = [];
+  for (let i = 0; i < strArray.length; i++) {
+    let camilizedWordArray = "";
+    if (strArray[i] !== "") {
+      camilizedWordArray = strArray[i][0].toUpperCase() + strArray[i].slice(1);
+    } else {
+    }
+
+    result.push(camilizedWordArray);
+  }
+  console.log(result);
+  return (
+    result[0][0].toLowerCase() + result[0].slice(1) + result.slice(1).join("")
+  ); //+ result[0].slice(1) + result.slice(0);
+}
+
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("bla-bla-bla-bla-bla-bla"));
+console.log(camelize("-webkit-transition"));
