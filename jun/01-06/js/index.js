65a9740c32d63d166d2727ccea6158be78d94830
hnsreeny <hnsreeny@gmail.com>
2022-06-01 08:57:55 +0200

    Node list in DOM

---
+++ jun/01-06/js/index.js
@@ -0,0 +1,10 @@
+// window.alert === alert()
+
+// -Node-
+// In the context of the DOM, a node is a single point in the node tree. Various things that are nodes are the document itself, elements, text, and comments.
+
+// document.querySelectorAll() will return A non-live NodeList 🧐
+// -NodeList-
+// NodeList objects are collections of nodes, usually returned by properties such as Node.childNodes and methods such as document.querySelectorAll().
+
+// Node tree https://dom.spec.whatwg.org/#concept-node
