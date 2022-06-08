16cdb46c3335c632842f43c9bc1ff66c57de9473
hnsreeny <hnsreeny@gmail.com>
2022-06-08 09:46:51 +0200

    Dom events

--- jun/08-06/js/index.js
+++ jun/08-06/js/index.js
@@ -1,7 +1,11 @@
 // password gen upon a user input
 const box = document.querySelector(".box");
 
-const changeBg = () => {
+const whiteBg = () => {
+  box.style.backgroundColor = "white";
+};
+const orangeBg = () => {
   box.style.backgroundColor = "orange";
 };
-box.addEventListener("click", changeBg);
+box.addEventListener("mouseenter", orangeBg);
+box.addEventListener("mouseleave", whiteBg);
