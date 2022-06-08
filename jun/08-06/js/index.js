325533cc7980bf945175a6ca5fa57d61fd4aecb8
hnsreeny <hnsreeny@gmail.com>
2022-06-08 10:54:30 +0200

    Dom events

--- jun/08-06/js/index.js
+++ jun/08-06/js/index.js
@@ -1,11 +1,32 @@
 // password gen upon a user input
+// var
+const arr = [];
+// html elements
 const box = document.querySelector(".box");
-
+const h2 = document.querySelector(".result h2");
+const userData = document.querySelector("#user-name");
+const form = document.querySelector("form");
+// functions
 const whiteBg = () => {
   box.style.backgroundColor = "white";
 };
 const orangeBg = () => {
   box.style.backgroundColor = "orange";
 };
+// EventListeners
+box.addEventListener("click", () => console.log("cool"));
 box.addEventListener("mouseenter", orangeBg);
 box.addEventListener("mouseleave", whiteBg);
+
+const sayHi = () => {
+  // alert("Hey, happy to have you again");
+};
+window.addEventListener("load", sayHi);
+// userData.addEventListener("keyup", () => console.log(userData.value));
+const newUser = (e) => {
+  e.preventDefault();
+  console.log(e);
+  h2.innerHTML += `${userData.value} <br>`;
+};
+form.addEventListener("submit", newUser);
+//form.addEventListener("reset",()=>);
