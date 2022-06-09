9de7dee29b65ac8971b5dea88ffdf513d43ae5d4
hnsreeny <hnsreeny@gmail.com>
2022-06-09 09:38:39 +0200

    Win Events

--- jun/09-06/js/index.js
+++ jun/09-06/js/index.js
@@ -1,8 +1,23 @@
 // password gen upon a user input
+const pass = document.querySelector("#pass");
+let userKeyWords = "";
 const passGen = (e) => {
   e.preventDefault();
+  userKeyWords += "1234567890!$@€&HADI";
+  let result = "";
+  for (let i = 0; i < 8; i++) {
+    result += userKeyWords[Math.floor(Math.random() * userKeyWords.length)];
+    if (i % 4 == 0) {
+      result += "-";
+    }
+  }
+  pass.innerHTML = result;
 };
-const storeValue = () => {};
-document.querySelector("#key-words").addEventListener("change", storeValue);
+const storeValue = (e) => {
+  console.log(e);
+  userKeyWords = e.target.value;
+};
+
+document.querySelector("#key-words").addEventListener("keyup", storeValue);
 document.querySelector("form").addEventListener("submit", passGen);
 // Game :)
