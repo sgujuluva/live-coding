9e5d1dada7879ac2abaeb5818be3899613114818
Andrej Tacigin <andrej.tacigin@digitalcareerinstitute.org>
2022-06-14 12:54:35 +0200

    add event delegation

--- jun/14-06/EventDelegation/js/index.js
+++ jun/14-06/EventDelegation/js/index.js
@@ -1,9 +1,12 @@
 const listItems = document.querySelectorAll("li");
 const buttons = document.querySelectorAll("button");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
 
-// Array of listItems
+// Dummy example of listItems
 // [{ htmlTag: 'li', text: 'First' } , { htmlTag: 'li', text: 'Second'}]
-// Array of buttons
+// Dummy example of buttons
 // [{ htmlTag: 'button', text: 'Delete'}, { htmlTag: 'button', text: 'Delete'}, { htmlTag: 'button', text: 'Delete'}]
 
 // ! Do not use this way of adding event listeners to both the buttons and the li's
@@ -17,18 +20,48 @@ const buttons = document.querySelectorAll("button");
 //   });
 // });
 
-// Adding event listeners to list items
-for (let li of listItems) {
-  li.addEventListener("click", () => {
-    console.log("I am an li");
-  });
-}
-
 // Adding event listeners to buttons and stopping propagation
-for (let btn of buttons) {
-  btn.addEventListener("click", (event) => {
-    // Stopping propagation prevents the event bubbling
-    event.stopPropagation();
-    console.log("I am a btn");
-  });
-}
+// for (let btn of buttons) {
+//   btn.addEventListener("click", (event) => {
+//     Stopping propagation prevents the event bubbling
+// !   event.stopPropagation();
+//     console.log("I am a btn");
+//   });
+// }
+
+// Adding event listeners to list items
+// for (let li of listItems) {
+//   li.addEventListener(
+//     "click",
+//     () => {
+//       console.log("I am an li");
+//     }
+//    Event capturing - reverse the order of event triggering
+//    The parent event will trigger before the child event
+// !  true
+//   );
+// }
+
+// Event listener for the button to add a new list item
+button.addEventListener("click", () => {
+  const newListItem = document.createElement("li");
+  newListItem.innerText = input.value;
+  const btn = document.createElement("button");
+  btn.innerText = "Delete";
+  newListItem.append(btn);
+  list.append(newListItem);
+});
+
+// ! Event delegation
+// Adding an event listener to the parent element
+list.addEventListener("click", (event) => {
+  // If the target that was clicked is a list item
+  if (event.target.matches("li")) {
+    console.log("Li clicked");
+  }
+
+  // If the target that was clicked is a button
+  if (event.target.matches("button")) {
+    list.removeChild(event.target.parentElement);
+  }
+});
