737dc7576d0c6cbd162056245b6681c10f4377fd
tareq-almasri <talmasreq@gmail.com>
2022-07-07 10:06:15 +0200

    solution added

---
+++ jul/07-07/exercise/my-app/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function App() {
+  const [numsArr, setNumsArr] = useState([]);
+
+  const handleClick = () => {
+    let newArr = [];
+
+    while (newArr.length < 6) {
+      const randomNum = Math.floor(Math.random() * 49 + 1);
+      if (!newArr.includes(randomNum)) {
+        newArr.push(randomNum);
+      }
+
+      // newArr.push(randomNum);
+
+      //  new Set ([...newArr])
+    }
+
+    const superNum = Math.floor(Math.random() * 10 + 1);
+
+    newArr.push(superNum);
+
+    setNumsArr(newArr);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        {numsArr.map((item, i) => (
+          <div className="num" key={i}> {item} </div>
+        ))}
+      </div>
+      <div>
+        <button onClick={() => setNumsArr([])}> Reset</button>
+        <button onClick={handleClick}> get Lucky Nums</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
